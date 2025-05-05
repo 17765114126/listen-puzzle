05b0170593f7c54087b63c86828e4d8bef3ba530
zf <17765114126@163.com>
2025-05-05 23:23:51 +0800

    语音克隆

--- api/svc_api.py
+++ api/svc_api.py
@@ -2,6 +2,8 @@ from data import dh_live
 from fastapi import APIRouter
 from api.Do import BaseReq
 import config
+from data.sovits_v4 import use_sovits_v4
+import soundfile as sf
 
 router = APIRouter()
 
@@ -29,4 +31,33 @@ async def merge_audio(req: BaseReq):
     return {
         "finalUrl": f"{access_url_path}{finalUrl}",
         "finalWebUrl": f"{config.UPLOAD_DIR}{finalUrl}",
+    }
+
+
+# 语音克隆
+@router.post("/sovits_v4")
+async def tts_endpoint(req: BaseReq):
+    opt_sr, audio_opt = use_sovits_v4.get_tts_wav(
+        ref_wav_path=req.ref_wav_path,
+        prompt_text=req.prompt_text,
+        prompt_language=req.prompt_language,
+        text=req.text,
+        text_language=req.text_language,
+        # how_to_cut=req.how_to_cut,
+        top_k=req.top_k,
+        top_p=req.top_p,
+        temperature=req.temperature,
+        # ref_free=ref_free,
+        speed=req.speed,
+        # if_freeze=if_freeze,
+        # inp_refs=inp_refs,
+        # sample_steps=sample_steps,
+        # if_sr=if_sr,
+        # pause_second=pause_second,
+    )
+    access_url_path = config.ROOT_DIR_WIN / "static" / "uploads" / 'sovits_v4_audio.wav'
+    sf.write(access_url_path, audio_opt, opt_sr)
+    return {
+        "vitsV4WebUrl": 'static/uploads/sovits_v4_audio.wav',
+        "vitsV4Url": access_url_path,
     }
---
+++ data/sovits_v4/text/g2pw/__init__.py
@@ -0,0 +1,1 @@
+from data.sovits_v4.text.g2pw.g2pw import *
