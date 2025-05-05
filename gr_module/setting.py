7b6bfcade5426f2992573a107dcd621d91c29a91
zf <17765114126@163.com>
2025-05-05 23:37:37 +0800

    del

--- gr_module/setting.py
+++
@@ -1,8 +0,0 @@
-import gradio as gr
-import config
-
-
-def setting():
-    with gr.Row():
-        device1 = gr.Dropdown(config.whisper_device, value="qwen",
-                              label="大模型选择")
